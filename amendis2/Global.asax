15efacad7114eaaa3f1c624b72b65d7a735a92d1
egris9 <amjadnaciri73@gmal.com>
2024-07-06 18:40:40 +0100

    Add project files.

---
+++ amendis2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="amendis2.Global" Language="C#" %>
---
+++ amendis2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(amendis2.Startup))]
+namespace amendis2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
